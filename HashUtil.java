81e48483ffea1df9500c7cf3a46063b01c6bae2f
heji0826 <heji0826@naver.com>
2025-01-01 22:50:27 +0900

    feat : add md5 hash java file

---
+++ HashUtil.java
@@ -0,0 +1,19 @@
+import java.security.MessageDigest;
+
+public class HashUtil {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("MD5 해싱 실패", e);
+        }
+    }
+}
